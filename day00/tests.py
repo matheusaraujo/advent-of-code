10989dffcb866c33bad2890c6ac99b546ba97f53
Matheus Araujo <matheus.saraujo@gmail.com>
2023-12-03 14:03:25 +0100

    refactor(template): refactoring template tests

--- day00/tests.py
+++ day00/tests.py
@@ -4,25 +4,28 @@ from problem1 import problem1
 from problem2 import problem2
 
 class Testing(unittest.TestCase):
-    def test_problem1_sample(self):
-        output = problem1("problem1")
-        self.assertEqual(output, "problem1")
-
-    def test_problem1(self):
+    def read_input(self):
         with open("input.txt") as f:
             input = f.readlines()
-        
+        return input
+
+    sample_input = "some input"
+
+    def test_problem1_sample(self):
+        output = problem1(self.sample_input)
+        self.assertEqual(output, "some input")
+
+    def test_problem1(self):
+        input = self.read_input()
         output = problem1(input)
         self.assertEqual(output, ["sample input"])
 
     def test_problem2_sample(self):
-        input = problem2("some input")
+        input = problem2(self.sample_input)
         self.assertEqual(input, "problem2")
 
     def test_problem2(self):
-        with open("input.txt") as f:
-            input = f.readlines()
-        
+        input = self.read_input()
         output = problem2(input)
         self.assertEqual(output, "problem2")
 
