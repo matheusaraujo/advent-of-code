31340620724e9be183d28b61be713ba321d337a4
Matheus Araujo <matheus.saraujo@gmail.com>
2023-12-04 14:31:28 +0100

    feat(2023): day 04

--- 2023/day04/problem2.py
+++ 2023/day04/problem2.py
@@ -1,2 +1,15 @@
+import re
+
 def problem2(input):
-    return "problem2"
+    cards = [1] * len(input)
+    for card_number in range(0, len(input)):
+        numbers = input[card_number].split(':')[1].split('|')
+        winning_numbers = [int(n) for n in numbers[0].split(' ') if len(n) > 0]
+        my_numbers = [int(n) for n in numbers[1].split(' ') if len(n) > 0]
+        
+        cnt = sum(1 for n in my_numbers if (n in winning_numbers))
+
+        for i in range(0, cnt):
+            cards[card_number+i+1] += cards[card_number]
+
+    return sum(cards)
--- 2023/day04/tests.py
+++ 2023/day04/tests.py
@@ -9,14 +9,14 @@ class Testing(unittest.TestCase):
             input = f.readlines()
         return input
 
-    sample_input = {
+    sample_input = [
         "Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53",
         "Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19",
         "Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1",
         "Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83",
         "Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36",
         "Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11",
-    }
+    ]
 
     def test_problem1_sample(self):
         output = problem1(self.sample_input)
@@ -27,14 +27,14 @@ class Testing(unittest.TestCase):
         output = problem1(input)
         self.assertEqual(output, 24848)
 
-    # def test_problem2_sample(self):
-    #     input = problem2(self.sample_input)
-    #     self.assertEqual(input, "problem2")
+    def test_problem2_sample(self):
+        input = problem2(self.sample_input)
+        self.assertEqual(input, 30)
 
-    # def test_problem2(self):
-    #     input = self.read_input()
-    #     output = problem2(input)
-    #     self.assertEqual(output, "problem2")
+    def test_problem2(self):
+        input = self.read_input()
+        output = problem2(input)
+        self.assertEqual(output, 7258152)
 
 if __name__ == '__main__':
     unittest.main()
