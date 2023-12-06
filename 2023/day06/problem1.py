0f907ed7b8fdb9dab3dfb964682fe923ceaad504
Matheus Araujo <matheus.saraujo@gmail.com>
2023-12-06 14:40:56 +0100

    refactor(2023): day 06

--- 2023/day06/problem1.py
+++ 2023/day06/problem1.py
@@ -1,5 +1,7 @@
 import re
 
+from helpers import get_integer_roots
+
 def extract_numbers(s):
     return [int(n) for n in re.findall(r'\d+', s)]
 
@@ -7,12 +9,9 @@ def problem1(input):
     times, distances, ans = extract_numbers(input[0]), extract_numbers(input[1]), 1
     
     for i in range(0, len(times)):
-        cnt = 0
-        for t in range(0, times[i]):
-            if t * (times[i] - t) > distances[i]: 
-                cnt += 1
-            elif cnt > 0: 
-                break
-        ans *= cnt
+        # Tt - t^2 > D
+        # - t^2 + Tt - D > 0
+        x1, x2 = get_integer_roots(-1, times[i], -distances[i])
+        ans *= (x2 - x1 - 1)
             
     return ans
