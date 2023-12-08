a2cf5d50c51442ae6e89f66266a8745c95eb3e0d
Matheus Araujo <matheus.saraujo@gmail.com>
2023-12-08 22:57:37 +0100

    refactor(2015): day 03

--- src/2015/day02/test_part1.py
+++ src/2015/day02/test_part1.py
@@ -6,8 +6,8 @@ actual_input = read_input_file("2015/day02/input.txt")
 
 class TestPart1:
     def test_with_sample_data(self):
-        assert part1(['2x3x4']) == 58
-        assert part1(['1x1x10']) == 43
+        assert part1(["2x3x4"]) == 58
+        assert part1(["1x1x10"]) == 43
 
     def test_with_actual_data(self):
         assert part1(actual_input) == 1598415
--- src/2015/day02/test_part2.py
+++ src/2015/day02/test_part2.py
@@ -6,8 +6,8 @@ actual_input = read_input_file("2015/day02/input.txt")
 
 class TestPart2:
     def test_with_sample_data(self):
-        assert part2(['2x3x4']) == 34
-        assert part2(['1x1x10']) == 14
+        assert part2(["2x3x4"]) == 34
+        assert part2(["1x1x10"]) == 14
 
     def test_with_actual_data(self):
         assert part2(actual_input) == 3812909
