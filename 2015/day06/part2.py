eb25b6d4056717cd4934bcf86bcd97df36470f4b
Matheus Araujo <matheus.saraujo@gmail.com>
2023-12-09 16:57:24 +0100

    refactor(2016): day 06

--- 2015/day06/part2.py
+++ 2015/day06/part2.py
@@ -1,26 +1,14 @@
-import re
+from helpers import solve
 
 
 def part2(puzzle_input):
-    m = [[0 for _ in range(1000)] for _ in range(1000)]
+    def turnon(x):
+        return x + 1
 
-    for line in puzzle_input:
-        ma = re.findall(r"(.* )(\d+),(\d+) through (\d+),(\d+)", line)[0]
-        op, x1, y1, x2, y2 = (
-            ma[0].strip(),
-            int(ma[1]),
-            int(ma[2]),
-            int(ma[3]),
-            int(ma[4]),
-        )
+    def turnoff(x):
+        return max(x - 1, 0)
 
-        for i in range(x1, x2 + 1):
-            for j in range(y1, y2 + 1):
-                if op == "turn on":
-                    m[i][j] += 1
-                elif op == "turn off":
-                    m[i][j] = max(m[i][j] - 1, 0)
-                else:
-                    m[i][j] += 2
+    def toggle(x):
+        return x + 2
 
-    return sum(map(sum, m))
+    return solve(puzzle_input, turnon, turnoff, toggle)
