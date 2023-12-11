0c46b7e687f374402b2b4a466d6dab7c5946df08
Matheus Araujo <matheus.saraujo@gmail.com>
2023-12-11 18:03:22 +0100

    feat(2023): day 11 - cosmic expansion, part 1

---
+++ 2023/day11/test_part2.py
@@ -0,0 +1,13 @@
+# from inputs import read_input_file
+# from part2 import part2
+
+# sample_input = read_input_file("2023/day11/sample-input.txt")
+# actual_input = read_input_file("2023/day11/input.txt")
+
+
+# class TestPart2:
+#     def test_with_sample_data(self):
+#         assert part2(sample_input) == 2
+
+#     def test_with_actual_data(self):
+#         assert part2(actual_input) == 84
