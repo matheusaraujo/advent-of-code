a8740de0b765c139472dba5ecfa983e907ffa74f
Matheus Araujo <matheus.saraujo@gmail.com>
2023-12-11 12:09:33 +0100

    chore: showing execution time in pytest

--- conftest.py
+++ conftest.py
@@ -1,5 +1,23 @@
 import sys
+import time
+
+import pytest
 
 
 def pytest_sessionstart():
     sys.path.append("utils")
+
+
+def format_time(execution_time):
+    if execution_time >= 1:
+        return f"{execution_time:.2f}s"
+    return f"{(execution_time * 1000):.2f}ms"
+
+
+@pytest.hookimpl(hookwrapper=True, tryfirst=True)
+def pytest_runtest_call():
+    start_time = time.time()
+    yield
+    end_time = time.time()
+    execution_time = end_time - start_time
+    print(f"\033[3;90m(executed in {format_time(execution_time)}) \033[0m", end="")
