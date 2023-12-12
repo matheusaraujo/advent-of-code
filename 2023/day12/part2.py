980c43ddc5d4f38bb1d8f7d7669b3ff9ac88c275
Matheus Araujo <matheus.saraujo@gmail.com>
2023-12-12 18:49:13 +0100

    refactor(2023): day 12 - hot springs

--- 2023/day12/part2.py
+++ 2023/day12/part2.py
@@ -1,48 +1,53 @@
 memory = {}
 
 
-# a -> springs "???.###"
-# b -> groups (1,1,3)
-def recursive(a, b):
-    if (a, b) in memory:
-        return memory[(a, b)]
-    ans = 0
-    if len(a) == 0:
-        # no more springs, groups must be empty to be valid
-        ans = 1 if len(b) == 0 else 0
-    else:
-        if a[0] == ".":
-            ans = recursive(a[1:], b)
-        elif a[0] == "?":
-            ans = recursive("." + a[1:], b) + recursive("#" + a[1:], b)
-        elif a[0] == "#":
-            if len(b) == 0 or len(a) < sum(b):
-                # no more groups or less springs the sum of groups, invalid
-                ans = 0
-            elif any(c == "." for c in a[0 : b[0]]):
-                # there are any "." on next N springs, invalid (N = size of current group)
-                ans = 0
-            elif len(b) == 1:
-                ans = recursive(a[b[0] :], b[1:])
-            else:
-                if len(a) < b[0] + 1 or a[b[0]] == "#":
-                    # less springs than the current group size
-                    # or spring just after end of current group is "#", invalid
-                    ans = 0
-                else:
-                    ans = recursive(a[b[0] + 1 :], b[1:])
+def cached_recursive(springs, groups):
+    if (springs, groups) not in memory:
+        memory[(springs, groups)] = recursive(springs, groups)
+    return memory[(springs, groups)]
 
-        memory[(a, b)] = ans
 
-    return ans
+def should_keep_checking(springs, groups):
+    if len(groups) == 0:
+        return False
+    if len(springs) < sum(groups):
+        return False
+    if any(s == "." for s in springs[0 : groups[0]]):
+        return False
+    if len(groups) > 1 and springs[groups[0]] == "#":
+        return False
+
+    return True
+
+
+def recursive(springs, groups):
+    if len(springs) == 0:
+        return 1 if len(groups) == 0 else 0
+
+    if springs[0] == ".":
+        return cached_recursive(springs[1:], groups)
+
+    if springs[0] == "?":
+        return (cached_recursive("." + springs[1:], groups)) + (
+            cached_recursive("#" + springs[1:], groups)
+        )
+
+    if not should_keep_checking(springs, groups):
+        return 0
+
+    return (
+        (cached_recursive(springs[groups[0] :], groups[1:]))
+        if len(groups) == 1
+        else (cached_recursive(springs[groups[0] + 1 :], groups[1:]))
+    )
 
 
 def count_arrangement_part2(line):
-    [a, b] = line.split(" ")
-    a = "?".join([a] * 5)
-    b = tuple(int(x) for x in b.split(","))
-    b = b * 5
-    return recursive(a, b)
+    [springs, groups] = line.split(" ")
+    springs = "?".join([springs] * 5)
+    groups = tuple(int(x) for x in groups.split(","))
+    groups = groups * 5
+    return cached_recursive(springs, groups)
 
 
 def part2(puzzle_input):
