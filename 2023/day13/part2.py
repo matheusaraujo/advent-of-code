5fb4981ff1d0b5a2214ab757cd0f5ef758ec1495
Matheus Araujo <matheus.saraujo@gmail.com>
2023-12-13 13:25:23 +0100

    style(2023): day 13 - point of incidence

--- 2023/day13/part2.py
+++ 2023/day13/part2.py
@@ -1,22 +1,23 @@
 from helpers import solve
 
 
-def is_one_bit_difference(num1, num2):
-    xor = num1 ^ num2
+# https://www.geeksforgeeks.org/check-whether-two-numbers-differ-one-bit-position/
+def is_one_bit_difference(n1, n2):
+    xor = n1 ^ n2
     return xor != 0 and (xor & (xor - 1)) == 0
 
 
 def is_reflection(arr, x):
-    dx, c = 0, 0
+    dx, flag = 0, 0
 
     while x + dx < len(arr) and x - dx > 0:
         if arr[x + dx] != arr[x - dx - 1]:
-            if c > 0 or not is_one_bit_difference(arr[x + dx], arr[x - dx - 1]):
+            if flag > 0 or not is_one_bit_difference(arr[x + dx], arr[x - dx - 1]):
                 return False
-            c = 1
+            flag = 1
         dx += 1
 
-    return c == 1
+    return flag == 1
 
 
 def part2(puzzle_input):
