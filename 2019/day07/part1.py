b956a546dd6a6f8d6a2e16dd47bc8730d3a3e368
Matheus Araujo <matheus.saraujo@gmail.com>
2023-12-13 21:46:37 +0100

    feat(2019): day 07 - amplification circuit - part 1

---
+++ 2019/day07/part1.py
@@ -0,0 +1,18 @@
+from itertools import permutations
+
+from helpers import int_code
+
+
+def part1(puzzle_input):
+    program = [int(x) for x in puzzle_input.split(",")]
+
+    arrangements = list(permutations(range(0, 5)))
+
+    ans = 0
+    for arrangement in arrangements:
+        signal = 0
+        for i in range(5):
+            signal = int_code(program[:], [arrangement[i], signal])
+        ans = max(ans, signal)
+
+    return ans
