6c49905b7fc8b090add9fdf2db219be0b3aadb31
Matheus Araujo <matheus.saraujo@gmail.com>
2023-12-22 19:38:23 +0100

    feat(2023): day 22 - sand slabs (must be refactored)

--- 2023/day22/helpers.py
+++ 2023/day22/helpers.py
@@ -12,28 +12,46 @@ class Point:
         return f"({self.x},{self.y}) {self.z}"
 
 
-@dataclass
 class Brick:
-    def __init__(self, n, p1: Point, p2: Point):
+    def __init__(self, n, p1: Point, p2: Point, space):
         self.id = self.__number_to_string(n + 1)
-        # forcing p1 < p2, points have only one different dimension
         self.p1 = Point(min(p1.x, p2.x), min(p1.y, p2.y), min(p1.z, p2.z))
         self.p2 = Point(max(p1.x, p2.x), max(p1.y, p2.y), max(p1.z, p2.z))
 
-        self.dir_0 = (
-            self.p1.x == self.p2.x and self.p1.y == self.p2.y and self.p1.z == self.p2.z
-        )
-        self.dir_x = self.p1.x != self.p2.x
-        self.dir_y = self.p1.y != self.p2.y
-        self.dir_z = self.p1.z != self.p2.z
+        self.supporting = set()
+        self.supported_by = set()
 
-        self.supporting = []
-        self.supported_by = []
+        self.__place(space)
 
     def __str__(self):
         return f"{self.p1} ~ {self.p2}  <- {self.id}"
 
-    def place(self, space):
+    def slide_down(self, space):
+        should_slide_down, count = True, 0
+
+        while should_slide_down:
+            should_slide_down = self.p1.z != 1
+            for x in range(self.p1.x, self.p2.x + 1):
+                for y in range(self.p1.y, self.p2.y + 1):
+                    should_slide_down = (
+                        should_slide_down and (x, y, self.p1.z - 1) not in space
+                    )
+
+            if should_slide_down:
+                self.__slide_down_z(space)
+                count += 1
+
+        return 1 if count > 0 else 0
+
+    def calc_relations(self, space):
+        for x in range(self.p1.x, self.p2.x + 1):
+            for y in range(self.p1.y, self.p2.y + 1):
+                if (x, y, self.p2.z + 1) in space:
+                    self.supporting.add(space[(x, y, self.p2.z + 1)])
+                if (x, y, self.p1.z - 1) in space:
+                    self.supported_by.add(space[(x, y, self.p1.z - 1)])
+
+    def __place(self, space):
         for x in range(self.p1.x, self.p2.x + 1):
             for y in range(self.p1.y, self.p2.y + 1):
                 for z in range(self.p1.z, self.p2.z + 1):
@@ -45,56 +63,11 @@ class Brick:
                 for z in range(self.p1.z, self.p2.z + 1):
                     space.pop((x, y, z))
 
-    def slide_down(self, space):
-        should_slide_down = True
-        while should_slide_down:
-            if self.p1.z == 1:
-                should_slide_down = False
-            elif self.dir_z or self.dir_0:
-                if (self.p1.x, self.p1.y, self.p1.z - 1) in space:
-                    should_slide_down = False
-            elif self.dir_x:
-                for x in range(self.p1.x, self.p2.x + 1):
-                    if (x, self.p1.y, self.p1.z - 1) in space:
-                        should_slide_down = False
-                        break
-            elif self.dir_y:
-                for y in range(self.p1.y, self.p2.y + 1):
-                    if (self.p1.x, y, self.p1.z - 1) in space:
-                        should_slide_down = False
-                        break
-
-            if should_slide_down:
-                self.__slide_down_z(space)
-
-    def calc_relations(self, space):
-        supporting, supported_by = set(), set()
-        if self.dir_z or self.dir_0:
-            if (self.p1.x, self.p1.y, self.p2.z + 1) in space:
-                supporting.add(space[(self.p1.x, self.p1.y, self.p2.z + 1)])
-            if (self.p1.x, self.p1.y, self.p1.z - 1) in space:
-                supported_by.add(space[(self.p1.x, self.p1.y, self.p1.z - 1)])
-        elif self.dir_x:
-            for x in range(self.p1.x, self.p2.x + 1):
-                if (x, self.p1.y, self.p1.z + 1) in space:
-                    supporting.add(space[(x, self.p1.y, self.p1.z + 1)])
-                if (x, self.p1.y, self.p1.z - 1) in space:
-                    supported_by.add(space[(x, self.p1.y, self.p1.z - 1)])
-        elif self.dir_y:
-            for y in range(self.p1.y, self.p2.y + 1):
-                if (self.p1.x, y, self.p1.z + 1) in space:
-                    supporting.add(space[(self.p1.x, y, self.p1.z + 1)])
-                if (self.p1.x, y, self.p1.z - 1) in space:
-                    supported_by.add(space[(self.p1.x, y, self.p1.z - 1)])
-
-        self.supported_by = list(supported_by)
-        self.supporting = list(supporting)
-
     def __slide_down_z(self, space):
         self.remove(space)
         self.p1.z -= 1
         self.p2.z -= 1
-        self.place(space)
+        self.__place(space)
 
     def __number_to_string(self, n):
         alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
@@ -106,3 +79,19 @@ class Brick:
             n = (n - 1) // 26
 
         return result
+
+
+def create_space(puzzle_input):
+    bricks, space = [], {}
+
+    for idx, line in enumerate(puzzle_input):
+        p1, p2 = line.split("~")
+        brick = Brick(idx, Point(*p1.split(",")), Point(*p2.split(",")), space)
+        bricks.append(brick)
+
+    bricks = sorted(bricks, key=lambda b: b.p1.z)
+
+    for brick in bricks:
+        brick.slide_down(space)
+
+    return bricks, space
--- 2023/day22/part2.py
+++ 2023/day22/part2.py
@@ -1,2 +1,19 @@
+import copy
+
+from helpers import create_space
+
+
 def part2(puzzle_input):
-    return int(puzzle_input[0]) * 2
+    bricks, space = create_space(puzzle_input)
+
+    ans = 0
+
+    for brick in bricks:
+        ss = dict(space)
+        brick.remove(ss)
+
+        for brick2 in copy.deepcopy(bricks):
+            if brick.id != brick2.id:
+                ans += brick2.slide_down(ss)
+
+    return ans
