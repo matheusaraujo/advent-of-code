8eb05aa1bf8299cde7d11f9b3a27e9f0cc603f9a
Matheus Araujo <matheus.saraujo@gmail.com>
2023-12-23 11:42:47 +0100

    feat(2023): day 23 - a long walk, part 1

---
+++ 2023/day23/part2.py
@@ -0,0 +1,49 @@
+import sys
+
+sys.setrecursionlimit(10000)
+
+directions = [
+    (1, 0, [".", "v", ">", "^", "<"]),
+    (-1, 0, [".", "v", ">", "^", "<"]),
+    (0, 1, [".", "v", ">", "^", "<"]),
+    (0, -1, [".", "v", ">", "^", "<"]),
+]
+
+
+def get_neighbors(graph, node):
+    neighbors = []
+    for d in directions:
+        x, y = node[0] + d[0], node[1] + d[1]
+        if graph[x][y] != "#":
+            neighbors.append((x, y))
+
+    return neighbors
+
+
+def dfs(graph, current, target, path):
+    if current == target:
+        path += [current]
+        return path
+
+    if current in path:
+        return path
+
+    max_path = path
+
+    for neighbor in get_neighbors(graph, current):
+        new_path = dfs(graph, neighbor, target, path + [current])
+        if len(new_path) > len(max_path) and new_path[-1] == target:
+            max_path = new_path
+
+    return max_path
+
+
+def part2(puzzle_input):
+    source, target = (0, puzzle_input[0].index(".")), (
+        len(puzzle_input) - 1,
+        puzzle_input[-1].index("."),
+    )
+
+    max_path = dfs(puzzle_input, source, target, [])
+
+    return len(max_path) - 1
---
+++ 2023/day23/test_part1.py
@@ -0,0 +1,13 @@
+# from inputs import read_input_file
+# from part1 import part1
+
+# sample_input = read_input_file("2023/day23/sample-input.txt")
+# actual_input = read_input_file("2023/day23/input.txt")
+
+
+# class TestPart1:
+#     def test_with_sample_data(self):
+#         assert part1(sample_input) == 94
+
+#     def test_with_actual_data(self):
+#         assert part1(actual_input) == 2294
