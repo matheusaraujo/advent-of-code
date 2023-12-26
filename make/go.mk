9caecdd94ff4eb80720b92732a62010009249e36
Matheus Araujo <matheus.saraujo@gmail.com>
2023-12-26 18:19:16 +0100

    chore: refactoring go entrypoint

--- make/go.mk
+++ make/go.mk
@@ -1,5 +1,5 @@
 define run-go
-	@ln -sf $(CURDIR)/$(year)/day$(day)/part1.go $(CURDIR)/aoc/go/part1.go
-	@ln -sf $(CURDIR)/$(year)/day$(day)/part2.go $(CURDIR)/aoc/go/part2.go
-	@go run aoc/go/part1.go aoc/go/part2.go aoc/go/run.go $(year) $(day)
+	@ln -sf ${CURDIR}/${year}/day${day}/part1.go ${CURDIR}/aoc/go/part1.go
+	@ln -sf ${CURDIR}/${year}/day${day}/part2.go ${CURDIR}/aoc/go/part2.go
+	@go run aoc/go/part1.go aoc/go/part2.go aoc/go/run.go ${year} ${day}
 endef
