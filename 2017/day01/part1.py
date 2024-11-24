a663d36307c16eff1ea4ba45c360d102a62e812c
Matheus Araujo <matheus.saraujo@gmail.com>
2024-11-24 11:07:33 +0000

    fix(2017): day 01 - linting and testing

--- 2017/day01/part1.py
+++ 2017/day01/part1.py
@@ -1,6 +1,6 @@
 def part1(puzzle_input):
     result, digits = 0, puzzle_input[0]
-    for i in range(len(digits)):
+    for i, _ in enumerate(digits):
         if digits[i] == digits[i - 1]:
             result += int(digits[i])
 
--- 2017/day01/part2.py
+++ 2017/day01/part2.py
@@ -2,7 +2,7 @@ def part2(puzzle_input):
     digits = puzzle_input[0]
     diff, result = len(digits) // 2, 0
 
-    for i in range(len(digits)):
+    for i, _ in enumerate(digits):
         if digits[i] == digits[(i + diff) % len(digits)]:
             result += int(digits[i])
 
