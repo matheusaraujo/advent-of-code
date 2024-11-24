dcd81b16f8188fcb422b1fded2ec12e0a1fa543d
Matheus Araujo <matheus.saraujo@gmail.com>
2024-11-24 17:29:18 +0000

    feat(2018): day 02 - inventory management system [warming up for 2024]

---
+++ 2018/day02/part1.pl
@@ -0,0 +1,31 @@
+#!/usr/bin/perl
+use strict;
+use warnings;
+
+sub part1 {
+    my @input = @_;
+    my ( $count2, $count3 ) = ( 0, 0 );
+
+    foreach my $word (@input) {
+        my %letters;
+        foreach my $letter ( split //sm, $word ) {
+            $letters{$letter}++;
+        }
+        my ( $has2, $has3 ) = ( 0, 0 );
+
+        foreach my $letter ( keys %letters ) {
+            if ( $letters{$letter} == 2 ) {
+                $has2 = 1;
+            }
+            if ( $letters{$letter} == 3 ) {
+                $has3 = 1;
+            }
+        }
+        $count2 += $has2;
+        $count3 += $has3;
+    }
+
+    return $count2 * $count3;
+}
+
+1;
---
+++ 2018/day02/part2.pl
@@ -0,0 +1,37 @@
+#!/usr/bin/perl
+use strict;
+use warnings;
+
+sub part2 {
+
+    my @input = @_;
+    my $size  = scalar @input;
+
+    foreach my $i ( 0 .. $size - 1 ) {
+        foreach my $j ( $i + 1 .. $size - 1 ) {
+
+            my $diff = 0;
+            my $common;
+            foreach my $k ( 0 .. length( $input[$i] ) - 2 ) {
+
+                if ( ( substr $input[$i], $k, 1 ) ne
+                    ( substr $input[$j], $k, 1 ) )
+                {
+                    $diff++;
+                }
+                else {
+                    $common .= substr $input[$i], $k, 1;
+                }
+
+            }
+            if ( $diff == 1 ) {
+                return $common;
+            }
+        }
+    }
+
+    return 'fgij';
+}
+
+1;
+
