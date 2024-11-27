199ec5e70a8abcc6e8fdf6352b3cc50d4bbda112
Matheus Araujo <matheus.saraujo@gmail.com>
2024-11-27 19:24:22 +0000

    feat(python): supporting requirements file

--- lib/python/run.sh
+++ lib/python/run.sh
@@ -6,4 +6,8 @@ part=$3
 input_file=$4
 output_file=$5
 
+if [ -f "$year/day$day/requirements.txt" ]; then
+    pip install -q -r "$year/day$day/requirements.txt"
+fi
+
 python3 ./lib/python/main.py $year $day $part $input_file $output_file
