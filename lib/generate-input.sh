5883fe5c69fb337f073bb527e11c0f02db677495
Matheus Araujo <matheus.saraujo@gmail.com>
2024-11-29 19:55:39 +0000

    feat(aoc-dx): script for generate input

---
+++ lib/generate-input.sh
@@ -0,0 +1,18 @@
+#/bin/bash
+
+year=$1
+day=$2
+part=$3
+
+letter=$(ls $year/day$day/data/input.$part.*.txt 2>/dev/null | wc -l | awk '{printf "%c", 97 + $1}')
+
+echo "input:"
+read input
+
+mkdir -p $year/day$day/data
+echo "$input" > $year/day$day/data/input.$part.$letter.txt
+
+echo "output:"
+read output
+
+echo "$output" > $year/day$day/data/output.$part.$letter.txt
