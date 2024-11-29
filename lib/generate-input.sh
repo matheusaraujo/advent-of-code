4ee318d71fab14c768d8257d597b15a3565d58d8
Matheus Araujo <matheus.saraujo@gmail.com>
2024-11-29 20:40:32 +0000

    fix(aoc-dx): generate-input, supporting multiline for input

--- lib/generate-input.sh
+++ lib/generate-input.sh
@@ -6,8 +6,8 @@ part=$3
 
 letter=$(ls $year/day$day/data/input.$part.*.txt 2>/dev/null | wc -l | awk '{printf "%c", 97 + $1}')
 
-echo "input:"
-read input
+echo "input(Ctrl+D to finish):"
+input=$(cat)
 
 mkdir -p $year/day$day/data
 echo "$input" > $year/day$day/data/input.$part.$letter.txt
