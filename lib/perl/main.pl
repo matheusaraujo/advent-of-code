f97237ec101fdf529fc9c13b75f86e2f92697dc3
Matheus Araujo <matheus.saraujo@gmail.com>
2024-11-30 01:42:20 +0100

    feat(aoc-dx): moving validation from main.lang to run.sh (#65)

--- lib/perl/main.pl
+++ lib/perl/main.pl
@@ -1,81 +1,17 @@
 #!/usr/bin/perl
 use strict;
 use warnings;
-
-use Time::HiRes qw(time);
-use feature 'say';
 use Carp;
 
+my ( $year, $day, $part ) = @ARGV;
+my $solution_path = "./$year/day$day/$part.pl";
+my $func          = \&{$part};
+
+do $solution_path or croak "Could not load script '$solution_path': $!";
+
 sub main {
-    my ( $year, $day, $part_name, $input_file, $output_file ) = @ARGV;
-
-    my @input = read_file($input_file);
-    my $expected_output = $output_file ? read_file($output_file, scalar => 1) : undef;
-
-    my $script_path = get_script_path( $year, $day, $part_name );
-    load_script($script_path);
-
-    my $part_function = \&{$part_name};
-    execute_part( $part_name, $input_file, $part_function, \@input, $expected_output );
-}
-
-sub get_script_path {
-    my ( $year, $day, $part_name ) = @_;
-    return "./$year/day$day/$part_name.pl";
-}
-
-sub load_script {
-    my ($script_path) = @_;
-    do $script_path or croak "Could not load script '$script_path': $!";
-}
-
-sub read_file {
-    my ( $file_path, %opts ) = @_;
-    open my $fh, '<', $file_path or croak "Could not open file '$file_path': $!";
-    my @lines = <$fh>;
-    close $fh or croak "Could not close file '$file_path': $!";
-    return $opts{scalar} ? $lines[0] : @lines;
-}
-
-sub execute_part {
-    my ( $part_name, $input_file, $part_function, $input_ref, $expected_output ) = @_;
-
-    my $input_file_label = get_input_file_label($input_file);
-
-    my $start_time = time;
-    my $result     = $part_function->(@$input_ref);
-    my $end_time   = time;
-
-    my $elapsed_time = sprintf '%.4fms', ($end_time - $start_time) * 1000;
-    my ($result_check, $result_symbol) = get_result_symbol( $result, $expected_output );
-
-    say format_result_message( $part_name, $input_file_label, $result, $elapsed_time, $result_symbol );
-
-    if (!$result_check) {
-        exit 1;
-    }
-}
-
-sub get_input_file_label {
-    my ($input_file) = @_;
-    my $file_name = (split /\//, $input_file)[-1];
-    return $file_name eq 'input.txt' ? '' : '(' . (split /\./, $file_name)[-2] . ')';
-}
-
-sub get_result_symbol {
-    my ( $result, $expected_output ) = @_;
-    return (1, '') unless defined $expected_output;
-    my $is_equal = $result eq $expected_output;
-    my $symbol = $is_equal ? "\033[32m✔\033[0m" : "\033[91m✘ $expected_output\033[0m";
-    return ($is_equal, $symbol);
-}
-
-sub format_result_message {
-    my ( $part_name, $input_file_label, $result, $elapsed_time, $result_symbol ) = @_;
-    return sprintf(
-        "\033[35m%s%s: \033[0m\033[32m%s\033[3;90m (executed in %s)%s\033[0m",
-        $part_name, $input_file_label, $result, $elapsed_time, $result_symbol
-    );
+    my @input_data = <STDIN>;
+    print $func->(@input_data);
 }
 
 main();
--- lib/run.sh
+++ lib/run.sh
@@ -3,37 +3,83 @@
 year=$1
 day=$2
 
-run() {
+get_input_file_label() {
+    file_name=$(basename "$1")
+    if [ "$file_name" == "input.txt" ]; then
+        echo ""
+    else
+        IFS='.' read -ra parts <<< "$file_name"
+        label="(${parts[-2]})"
+        echo "$label"
+    fi
+}
+
+run_lang() {
     lang=$1
-    part=$2
-    
-    input_files=$(ls "$year/day$day/data/input.$part"* 2>/dev/null)
+    year=$2
+    day=$3
+    part=$4
+    input_file=$5
+    output_file=$6
 
-    if [ -n "$input_files" ]; then
-        for input_file in $input_files; do
-            output_file=$(echo "$input_file" | sed 's/input/output/')
-            if [ ! -f "$output_file" ]; then
-                echo "Error: Output file $output_file does not exist."
-                exit 1
-            fi  
-            lib/$lang/run.sh $year $day $part "$input_file" "$output_file"
+    start_time=$(date +%s%3N)
 
-            if [ $? -ne 0 ]; then
-                exit 1
-            fi
-        done
-    fi
-
-    if [ -f "$year/day$day/data/output.$part.txt" ]; then
-        lib/$lang/run.sh $year $day $part "$year/day$day/data/input.txt"  "$year/day$day/data/output.$part.txt"
-    else  
-        lib/$lang/run.sh $year $day $part "$year/day$day/data/input.txt"
-    fi
-
+    script_output=$(lib/$lang/run.sh "$year" "$day" "$part" "$input_file")
     if [ $? -ne 0 ]; then
         exit 1
     fi
+
+    end_time=$(date +%s%3N)
+    elapsed_time=$((end_time - start_time))
+
+    if [ -n "$output_file" ]; then
+        if [ ! -f "$output_file" ]; then
+            echo "Error: Output file $output_file does not exist."
+            exit 1
+        fi
+
+        expected_output=$(cat "$output_file")
+
+        if [ "$script_output" != "$expected_output" ]; then
+            echo -e "\033[35m$part$input_label: \033[0m\033[32m$script_output\033[3;90m (executed in ${elapsed_time}ms)\033[91m ✘ Expected: $expected_output \033[0m"
+            exit 1
+        else
+            result_symbol="\033[32m✔\033[0m"
+        fi
+    else
+        result_symbol=""
+    fi
+
+    input_label=$(get_input_file_label "$input_file")
+    echo -e "\033[35m$part$input_label: \033[0m\033[32m$script_output\033[3;90m (executed in ${elapsed_time}ms) $result_symbol\033[0m"
 }
+
+run() {
+    lang=$1
+    part=$2
+
+    input_files=$(ls "$year/day$day/data/input.$part"* 2>/dev/null)
+
+    for input_file in $input_files; do
+        output_file="${input_file/input/output}"
+        
+        if [ ! -f "$output_file" ]; then
+            echo "Error: Output file $output_file does not exist."
+            exit 1
+        fi
+        
+        run_lang $lang $year $day $part "$input_file" "$output_file"
+    done
+
+    output_txt="$year/day$day/data/output.$part.txt"
+    if [ -f "$output_txt" ]; then
+        run_lang $lang $year $day $part "$year/day$day/data/input.txt" "$output_txt"
+    else
+        run_lang $lang $year $day $part "$year/day$day/data/input.txt" ""
+    fi
+}
+
+
 
 for lang in python perl; do
     lib/$lang/created.sh $year $day
