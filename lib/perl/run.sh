131fc8dccee15bd8f204760f9d7f24b918b05b7a
Matheus Araujo <matheus.saraujo@gmail.com>
2024-11-30 00:41:15 +0000

    feat(aoc-dx): moving validation from main.lang to run.sh

--- lib/perl/run.sh
+++ lib/perl/run.sh
@@ -4,6 +4,5 @@ year=$1
 day=$2
 part=$3
 input_file=$4
-output_file=$5
 
-./lib/perl/main.pl $year $day $part $input_file $output_file
+./lib/perl/main.pl $year $day $part < $input_file
--- lib/python/run.sh
+++ lib/python/run.sh
@@ -4,10 +4,9 @@ year=$1
 day=$2
 part=$3
 input_file=$4
-output_file=$5
 
 if [ -f "$year/day$day/requirements.txt" ]; then
     pip install -q -r "$year/day$day/requirements.txt"
 fi
 
-python3 ./lib/python/main.py $year $day $part $input_file $output_file
+python3 ./lib/python/main.py $year $day $part < $input_file
