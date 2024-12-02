558d93ec4c32fa46d207a58a41275cb3195a059f
Matheus Araujo <matheus.saraujo@gmail.com>
2024-12-02 18:10:51 +0100

    feat(2024): Day 2: Red-Nosed Reports (#89)
    * feat(2024): Day 2: Red-Nosed Reports [missing fix perlcritic]
    
    * feat(2024): Day 2: Red-Nosed Reports

---
+++ 2024/day02/part2.pl
@@ -0,0 +1,32 @@
+#!/usr/bin/perl
+use strict;
+use warnings;
+
+sub part2 {
+    my @puzzle_input = @_;
+    my $safe_reports = 0;
+
+    foreach my $line (@puzzle_input) {
+        my @report = split m/\s/sxm, $line;
+        if ( is_safe_report(@report) ) {
+            $safe_reports++;
+        }
+        else {
+            for my $i ( 0 .. $#report ) {
+                my @left_report = $i > 0 ? @report[ 0 .. $i - 1 ] : ();
+                my @right_report =
+                  $i < $#report ? @report[ $i + 1 .. $#report ] : ();
+
+                if ( is_safe_report( @left_report, @right_report ) ) {
+                    $safe_reports++;
+                    last;
+                }
+            }
+        }
+    }
+
+    return $safe_reports;
+}
+
+1;
+
