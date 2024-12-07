8410b9b5ac9ae0fac087e5ca3a9c47612ebe5d7e
Matheus Araujo <matheus.saraujo@gmail.com>
2024-12-07 16:01:17 +0000

    chore(aoc-dx): including cpu and memory usage

--- lib/aoc/run.sh
+++ lib/aoc/run.sh
@@ -90,15 +90,48 @@ execute_solution_script() {
     local output_file=$6
 
     local start_time=$(date +%s%N)
-    local script_output=$(lib/$lang/run.sh "$year" "$day" "$part" "$input_file")
-    local end_time=$(date +%s%N)
-    local elapsed_time=$((end_time - start_time))
-    local elapsed_ms=$(echo "scale=3; $elapsed_time / 1000000" | bc)
 
-    if [ $? -ne 0 ]; then
+    /usr/bin/time -f "Max Memory: %M KB\nCPU Usage: %P" \
+        -o /tmp/resource_usage.txt -- \
+        lib/$lang/run.sh "$year" "$day" "$part" "$input_file" > /tmp/script_output.txt 2>&1
+    local script_exit_code=$?
+    local end_time=$(date +%s%N)
+
+    local elapsed_time=$((end_time - start_time))
+    local elapsed=""
+    local elapsed_ms=$(echo "scale=3; $elapsed_time / 1000000" | bc)
+    local elapsed_s=$(echo "scale=3; $elapsed_time / 1000000000" | bc)
+    local elapsed_min=$(echo "scale=3; $elapsed_time / 60000000000" | bc)
+
+    if (( $(echo "$elapsed_time < 1000000000" | bc -l) )); then
+        elapsed="${elapsed_ms}ms"
+    elif (( $(echo "$elapsed_time < 60000000000" | bc -l) )); then
+        elapsed="${elapsed_s}s"
+    else
+        elapsed="${elapsed_min}min"
+    fi
+
+    if [ $script_exit_code -ne 0 ]; then
+        echo "Script failed to execute." >&2
+        cat /tmp/script_output.txt >&2
         exit 1
     fi
+
+    local script_output=$(cat /tmp/script_output.txt)
+    local resource_usage=$(cat /tmp/resource_usage.txt)
+    local max_memory_kb=$(echo "$resource_usage" | grep "Max Memory" | awk '{print $3}')
+    local cpu_usage=$(echo "$resource_usage" | grep "CPU Usage" | awk '{print $3}')
+
     
+    local max_memory=""
+    if [ "$max_memory_kb" -ge 1048576 ]; then
+        max_memory=$(echo "scale=2; $max_memory_kb / 1048576" | bc)GB
+    elif [ "$max_memory_kb" -ge 1024 ]; then
+        max_memory=$(echo "scale=2; $max_memory_kb / 1024" | bc)MB
+    else
+        max_memory="${max_memory_kb}KB"
+    fi
+
     local result_symbol=""
     local input_label=$(generate_input_label "$input_file")
 
@@ -107,14 +140,16 @@ execute_solution_script() {
         local expected_output=$(cat "$output_file")
 
         if [ "$script_output" != "$expected_output" ]; then
-            print_success "\033[35m$part$input_label: \033[0m\033[32m$script_output\033[3;90m (executed in ${elapsed_ms}ms)\033[91m ✘ Expected: $expected_output \033[0m"
+            print_error "\033[35m$part$input_label: \033[0m\033[32m$script_output\033[3;90m (execution time: ${elapsed}, memory: ${max_memory}, cpu: ${cpu_usage})\033[91m ✘ Expected: $expected_output \033[0m"
             exit 1
         fi
         result_symbol="\033[32m✔\033[0m"
     fi
 
-    print_success "\033[35m$part$input_label: \033[0m\033[32m$script_output\033[3;90m (executed in ${elapsed_ms}ms) $result_symbol\033[0m"
+    print_success "\033[35m$part$input_label: \033[0m\033[32m$script_output\033[3;90m (execution time: ${elapsed}, memory: ${max_memory}, cpu: ${cpu_usage}) $result_symbol\033[0m"
 }
+
+
 
 validate_output_file() {
     local output_file=$1
