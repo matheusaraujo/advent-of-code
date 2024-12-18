9993acb19132fa2559db890d7be0d8e4ee948587
Matheus Araujo <matheus.saraujo@gmail.com>
2024-12-18 16:11:26 +0000

    chore: cleaning up code [skip ci]

--- 2024/day18/part2.pl
+++ 2024/day18/part2.pl
@@ -1,4 +1,5 @@
 # TODO: @matheusaraujo - brute force, this code needs to be refactored
+# suggestion: use binary search to find the answer between 1024 and the end of the input
 
 use strict;
 use warnings;
@@ -26,48 +27,5 @@ sub part2 {
     }
     return '-1,-1';
 }
-
-# sub dfs {
-
-#     my ( $size, $corrupted ) = @_;
-#     my @directions = ( [ 0, 1 ], [ 1, 0 ], [ 0, -1 ], [ -1, 0 ] );
-#     my @queue      = ( [ 0, 0, 0 ] );
-#     my %visited    = ();
-
-#     while (@queue) {
-
-#         my $current = shift @queue;
-#         my ( $x, $y, $steps ) = @{$current};
-
-#         $visited{"$x,$y"} = 1;
-
-#         if ( $x == $size - 1 && $y == $size - 1 ) {
-#             return $steps;
-#         }
-
-#         for my $direction (@directions) {
-#             my ( $dx, $dy ) = @{$direction};
-
-#             my $nx = $x + $dx;
-#             my $ny = $y + $dy;
-
-#             if (   $nx < 0
-#                 || $nx >= $size
-#                 || $ny < 0
-#                 || $ny >= $size
-#                 || exists $visited{"$nx,$ny"}
-#                 || exists $corrupted->{"$nx,$ny"} )
-#             {
-#                 next;
-#             }
-
-#             $visited{"$nx,$ny"} = 1;
-
-#             push @queue, [ $nx, $ny, $steps + 1 ];
-#         }
-#     }
-
-#     return -1;
-# }
 
 1;
